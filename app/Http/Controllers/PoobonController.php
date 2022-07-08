e59f55d2a22eb21b96e74af0b75a33989e27ce93
kasrasml <smlkasra@gmail.com>
2022-07-08 17:35:08 +0430

    TEST

---
+++ app/Http/Controllers/PoobonController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PoobonController extends Controller
+{
+    public function Poobon(){
+
+        return view ('public.layout.poobon');
+    }
+}
---
+++ resources/views/public/layout/poobon.blade.php
@@ -0,0 +1,9 @@
+@extends('public.layout.index')
+
+@section('content')
+
+
+
+<img src="https://photokade.com/wp-content/uploads/Poobon-photokade-com-2.jpg">
+
+@endsection
